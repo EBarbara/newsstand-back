68ac64f5858530bec569d663d53fda467b07b855
EBarbara <estevan.barbara@gmail.com>
2026-05-16 12:03:29 -0300

    feat: implement IssueViewSet with advanced filtering, pagination, and CBZ import functionality

--- core/views.py
+++ core/views.py
@@ -452,6 +452,7 @@ class MagazineViewSet(viewsets.ReadOnlyModelViewSet):
 class TagViewSet(viewsets.ModelViewSet):
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
+    lookup_field = 'slug'
 
 class SectionViewSet(viewsets.ModelViewSet):
     queryset = Section.objects.all().order_by('name')
